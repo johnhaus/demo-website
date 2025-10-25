f355266bf8f5e74d4570c3d6b5082a120fac7aed
John Haus <john.haus@outlook.com>
2025-10-25 12:46:19 -0400

    cleanup

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -29,26 +29,25 @@ const StyledNavLink = styled(NavLink)`
 	color: #D3D3D3;
 	transition: all 0.3s ease;
 	&:hover {
-	   text-shadow: 
-	     0 0 20px rgba(255, 255, 255, 1),
-	     0 0 25px rgba(255, 255, 255, 1);
-}
+	  text-shadow: 
+     	0 0 20px rgba(255, 255, 255, 1),
+     	0 0 25px rgba(255, 255, 255, 1);
+	}
 `;
 
 const Header = () => {
 
-	return (
-    	<HeaderWrapper>
-    		<div>Demo Website</div>
-			<NavLinks>
-	        <ul>
-	          <li><StyledNavLink to="/">Home</StyledNavLink></li>
-	          <li><StyledNavLink to="/about">About</StyledNavLink></li>
-	        </ul>
-	      </NavLinks>
-    	</HeaderWrapper>
-    )
-
+  return (
+	<HeaderWrapper>
+	  <div>Demo Website</div>
+	  <NavLinks>
+      	<ul>
+  		  <li><StyledNavLink to="/">Home</StyledNavLink></li>
+      	  <li><StyledNavLink to="/about">About</StyledNavLink></li>
+        </ul>
+      </NavLinks>
+	</HeaderWrapper>
+  )
 }
 
 export default Header
